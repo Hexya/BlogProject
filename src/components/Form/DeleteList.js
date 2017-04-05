dfbda2d95eb1ca170f6780253ad5ea90684b1734
loic-belaid <loicbere@gmail.com>
2017-04-05 12:58:54 +0200

    ad delete

--- src/components/Form/DeleteList.js
+++ src/components/Form/DeleteList.js
@@ -1,45 +1,45 @@
-// import React from 'react';
-// import Delete from './Delete.js'
-//
-//
-// class DeleteList extends React.Component {
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//       data:[]
-//     }
-//
-//   }
-//   componentDidMount(){
-//
-//     fetch(`${window.location.origin}/api`)
-//     .then((res) =>{
-//       return res.json();
-//     })
-//     .then((json) =>{
-//       this.setState({
-//         data:json
-//       });
-//     });
-//   }
-//
-//   renderCards(){
-//     return this.state.data.map((el, i) => {
-//       return(
-//        <Delete key={i} articleTitle={el.articleTitle} id={el._id}  />
-//       )
-//     })
-//   }
-//
-//   render() {
-//     let list = this.renderCards();
-//     return (
-//       <div className=' container d-flex flex-column justify-content-center'>
-//           {list}
-//       </div>
-//     );
-//   }
-//
-// }
-//
-// export default DeleteList;
+import React from 'react';
+import Delete from './Delete'
+
+
+class DeleteList extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data:[]
+    }
+
+  }
+  componentDidMount(){
+
+    fetch(`${window.location.origin}/api`)
+    .then((res) =>{
+      return res.json();
+    })
+    .then((json) =>{
+      this.setState({
+        data:json
+      });
+    });
+  }
+
+  renderCards(){
+    return this.state.data.map((el, i) => {
+      return(
+       <Delete key={i} articleTitle={el.articleTitle} id={el._id}  />
+      )
+    })
+  }
+
+  render() {
+    let list = this.renderCards();
+    return (
+      <div>
+          {list}
+      </div>
+    );
+  }
+
+}
+
+export default DeleteList;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import { Router, Route, hashHistory } from 'react-router';
 // import Login from './components/admin/Login.jsx';
 import Form from './components/Form/Form.js';
-import Delete from './components/Form/Delete.js';
+import DeleteList from "./components/Form/DeleteList";
 // import Plateform from './components/Plateform/Plateform.jsx';
 
 ReactDOM.render((
@@ -14,6 +14,6 @@ ReactDOM.render((
     {/* <Route path="/login" component={Login}/>
     <Route path="/plateform" component={Plateform}/> */}
     <Route path="/form" component={Form}/>
-    <Route path="/delete" component={Delete}/>
+    <Route path="/delete" component={DeleteList}/>
   </Router>
 ), document.getElementById('root'))
